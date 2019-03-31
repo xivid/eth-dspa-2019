ca0ca508db4df530d09eeb6e3cbbfbe636d0d4fb
Zhifei Yang <zhiyang@student.ethz.ch>
2019-03-31 13:51:59 +0200

    task 2

--- session-5-custom-operators/average/src/main.rs
+++ session-5-custom-operators/average/src/main.rs
@@ -8,17 +8,19 @@ fn main() {
         let mut count = 0.0;
 
         let input = (0..10).to_stream(scope);
-        input.unary(Pipeline, "Average", move |_, _| move |input, output| {
+        input.unary(Pipeline, "Average", move |_, _| {
                 let mut vector = Vec::new();
-                input.for_each(|time, data| {
-                    data.swap(&mut vector);
-                    let mut session = output.session(&time);
-                    for datum in vector.drain(..) {
-                        sum += datum as f64;
-                        count += 1.0;
-                        session.give((datum, sum / count))
-                    }
-                });
+                move |input, output| {
+                    input.for_each(|time, data| {
+                        data.swap(&mut vector);
+                        let mut session = output.session(&time);
+                        for datum in vector.drain(..) {
+                            sum += datum as f64;
+                            count += 1.0;
+                            session.give((datum, sum / count))
+                        }
+                    });
+                }
             })
             .inspect(|x| println!("seen: {:?}", x));
     });
--- session-5-custom-operators/submission.md
+++ session-5-custom-operators/submission.md
@@ -19,13 +19,23 @@ use timely::dataflow::operators::{Inspect, ToStream};
 use timely::dataflow::operators::generic::operator::Operator;
 fn main() {
     timely::example(|scope| {
-        let mut sum = 0;
+        let mut sum = 0.0;
+        let mut count = 0.0;
 
         let input = (0..10).to_stream(scope);
-        input.unary(/* TODO(A) */, "Average", move |_, _| move |input, output| {
-                input.for_each(|time, data| {
-                    /* TODO(B): operator logic goes here */
-                });
+        input.unary(Pipeline, "Average", move |_, _| {
+                let mut vector = Vec::new();
+                move |input, output| {
+                    input.for_each(|time, data| {
+                        data.swap(&mut vector);
+                        let mut session = output.session(&time);
+                        for datum in vector.drain(..) {
+                            sum += datum as f64;
+                            count += 1.0;
+                            session.give((datum, sum / count))
+                        }
+                    });
+                }
             })
             .inspect(|x| println!("seen: {:?}", x));
     });
